cc6fb70629ab73d9e8ea55d17e4363eaf8c64baf
Dmitry Yashenkov <repulthezth@gmail.com>
2026-03-08 00:15:31 +0500

    0.8

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,35 +1,35 @@
-import {createComparison, defaultRules} from "../lib/compare.js";
+import { createComparison, defaultRules } from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
 const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
-    Object.keys(indexes)
-        .forEach((elementName) => {
-            elements[elementName].append(
-                ...Object.values(indexes[elementName])
-                    .map(name => {
-                        const option = document.createElement('option');
-                        option.value = name;
-                        option.textContent = name;
-                        return option;
-                    })
-            );
-        });
+  // @todo: #4.1 — заполнить выпадающие списки опциями
+  Object.keys(indexes).forEach((elementName) => {
+    elements[elementName].append(
+      ...Object.values(indexes[elementName]).map((name) => {
+        const option = document.createElement("option");
+        option.value = name;
+        option.textContent = name;
+        return option; // важно вернуть option
+      })
+    );
+  });
 
-    return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
-        if (action && action.name === 'clear') {
-            const input = action.parentElement.querySelector('input');
-            const fieldName = action.dataset.field;
-            if (input) {
-                input.value = '';
-                state[fieldName] = '';
-            }
-        }
+  return (data, state, action) => {
+    // @todo: #4.2 — обработать очистку поля
+    if (action && action.name === "clear") {
+      const parent = action.parentElement;
+      const inp = parent.querySelector("input");
+      const field = action.dataset.field;
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+      if (inp) {
+        inp.value = "";
+        state[field] = "";
+      }
     }
+
+    // @todo: #4.5 — отфильтровать данные используя компаратор
+    return data.filter((row) => compare(row, state));
+  };
 }
