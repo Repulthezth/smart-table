20cad2aec8435f414f475c64f57e28ffab90cd2a
Dmitry Yashenkov <repulthezth@gmail.com>
2026-03-08 00:18:32 +0500

    0.9

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -5,29 +5,33 @@ const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
   // @todo: #4.1 — заполнить выпадающие списки опциями
-  Object.keys(indexes).forEach((elementName) => {
-    elements[elementName].append(
-      ...Object.values(indexes[elementName]).map((name) => {
-        const option = document.createElement("option");
-        option.value = name;
-        option.textContent = name;
-        return option; // важно вернуть option
-      })
-    );
-  });
+  Object.keys(indexes) 
+    .forEach((elementName) => {
+      elements[elementName].append(
+        ...Object.values(indexes[elementName])
+          .map((name) => {
+            const option = document.createElement("option");
+            option.value = name;
+            option.textContent = name;
+            const select = document.getElementById("filter");
+            return select.appendChild(option);
+          }),
+      );
+    });
 
   return (data, state, action) => {
     // @todo: #4.2 — обработать очистку поля
-    if (action && action.name === "clear") {
-      const parent = action.parentElement;
-      const inp = parent.querySelector("input");
-      const field = action.dataset.field;
+    const buttons = document.querySelectorAll('button[name="clear"]');
 
-      if (inp) {
+    buttons.forEach((button) => {
+      const parent = button.parentElement;
+      const inp = parent.querySelector("input");
+
+      button.addEventListener("click", () => {
         inp.value = "";
-        state[field] = "";
-      }
-    }
+        state[button.dataset.field] = "";
+      });
+    });
 
     // @todo: #4.5 — отфильтровать данные используя компаратор
     return data.filter((row) => compare(row, state));
